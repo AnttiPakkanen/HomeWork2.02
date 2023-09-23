ebb3f7d75077da191a2bf0efe7f09f60a0f2468e
AnttiPakkanen <a.pakkanen@me.com>
2023-09-24 02:02:20 +0600

    1 save

--- HomeWork2.02/ViewController.swift
+++ HomeWork2.02/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
+private enum TrafficLight {
+    case redLight
+    case yellowLight
+    case greenLight
+    case trafficLightOff
+}
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var trafficLightButton: UIButton!
+    
+    private var trafficLight = TrafficLight.redLight
+    
+    override func viewDidLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = redLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = redLightView.frame.width / 2
+    
     }
 
+    override func viewDidLoad() {
+        trafficLightButton.setTitle("START", for: .normal)
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+    }
 
+    @IBAction func trafficLightsButton() {
+        trafficLightButton.setTitle("NEXT", for: .normal)
+        
+        switch trafficLight {
+        case .redLight:
+            redLightView.alpha = 1
+            //yellowLightView.alpha = 0.3
+            //greenLightView.alpha = 0.3
+            trafficLight = .yellowLight
+        case .yellowLight:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            //greenLightView.alpha = 0.3
+            trafficLight = .greenLight
+        case .greenLight:
+            //redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            trafficLight = .trafficLightOff
+        case .trafficLightOff:
+            //redLightView.alpha = 0.3
+            //yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            trafficLight = .redLight
+            trafficLightButton.setTitle("START", for: .normal)
+        }
+    }
+    
+    
 }
 
